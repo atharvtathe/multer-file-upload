8112dace3adf2c90dd71962fbffb405aebaa7396
atharvtathe <atharvtathe1@gmail.com>
2021-04-17 16:52:16 +0530

    send image

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const multer  = require('multer');
 const { v4: uuidv4 } = require('uuid');
-
+const path = require('path');
 
 
 
@@ -12,9 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use('/images',express.static('uploads'));
+
 const storage = multer.diskStorage({
   destination: function(req, file, callback) {
-    callback(null, __dirname + '/uploads');
+    // callback(null, __dirname + '/uploads');
+    callback(null, './uploads');
   },
   filename: function (req, file, callback) {
     let fileFormat = file.mimetype.split('/');
@@ -22,14 +25,25 @@ const storage = multer.diskStorage({
   }
 });
 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+}
 
-const upload = multer({storage : storage});
+const upload = multer({storage : storage, fileFilter : fileFilter});
 
 app.post('/uploadfile' ,upload.single('myFile'),  (req, res) => {
     const file = req.file;
-    console.log(file.mimetype);
-    res.send(req.file);
-    
+    // console.log(file.mimetype);
+    // res.send(req.file);
+    res.send(`
+    <img src="/images/${file.filename}" alt="filename">
+    `);
+
 });
 
 app.get('/' , (req, res ) => {
